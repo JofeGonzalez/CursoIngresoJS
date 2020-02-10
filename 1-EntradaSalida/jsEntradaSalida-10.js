0c93661232f3feb6d42263c294562945f02946cf
JofeGonzalez <60555982+JofeGonzalez@users.noreply.github.com>
2020-02-10 18:56:09 -0300

    Decimo Commit
    En este también se encuentra el 11 ejercicio que no esta en la carpeta

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,29 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+//	var importe;
+//	var resultado;
+
+//	importe=document.getElementById('importe').value;
+//	importe=parseInt(importe);
+//	resultado=(importe-((importe*25)/100))
+//	document.getElementById('resultado').value=resultado;
+
+	var importe;
+	var porcentaje;
+	var resultado;
+
+
+	importe=document.getElementById('importe').value;
+	importe=parseInt(importe);
+	porcentaje=prompt("Ingrese el porcentaje");
+	porcentaje=parseInt(porcentaje);
+
+	if(porcentaje >= 0){
+		resultado=(importe+(importe*porcentaje/100));
+	}
+	if(porcentaje <= 0){
+		resultado=(importe+(importe*porcentaje/100));
+	}
+	document.getElementById('resultado').value=resultado;
 }
