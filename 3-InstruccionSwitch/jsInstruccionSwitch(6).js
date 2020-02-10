8c011ce79ec0e1c502db46da5a08d249a439f021
JofeGonzalez <60555982+JofeGonzalez@users.noreply.github.com>
2020-02-10 20:42:55 -0300

    Switch6

--- 3-InstruccionSwitch/jsInstruccionSwitch(6).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(6).js
@@ -1,8 +1,27 @@
 function mostrar()
 {
 //tomo la edad  
-var laHora = document.getElementById('hora').value;
-
-
-
+	var laHora = document.getElementById('hora').value;
+	if(laHora>-1 && laHora<7 || laHora>19 && laHora<25){
+			laHora=1;
+	}
+	if(laHora>6 && laHora<12){
+			laHora=2;
+	}
+	if(laHora>11 && laHora<20){
+			laHora=3;
+	}	
+	switch(laHora){
+		case 1:
+			alert("Es de noche.");
+			break;
+		case 2:
+			alert("Es de mañana.");
+			break;	
+		case 3:
+			alert("Es de tarde.");
+			break;	
+		default:
+			alert("la hora no existe.");	
+	}
 }//FIN DE LA FUNCIÓN
