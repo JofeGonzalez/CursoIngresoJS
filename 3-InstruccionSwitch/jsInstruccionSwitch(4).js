81515bb016446bf15998a3f6432800f653b29a52
JofeGonzalez <60555982+JofeGonzalez@users.noreply.github.com>
2020-02-10 20:41:59 -0300

    Switch4

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -4,8 +4,28 @@ function mostrar()
 var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
-	
-	
+	if(mesDelAño=='Enero' || mesDelAño=='Marzo' || mesDelAño=='Mayo' || mesDelAño=='Julio' || mesDelAño=='Agosto' || mesDelAño=='Octubre' || mesDelAño=='Diciembre'){
+		mesDelAño=1;	
+	}
+	if(mesDelAño=='Abril' || mesDelAño=='Junio' || mesDelAño=='Septiembre' || mesDelAño=='Noviembre'){
+		mesDelAño=2;	
+	}
+	if(mesDelAño=='Febrero'){
+		mesDelAño=3;	
+	}
+	switch(mesDelAño){
+		case 1:
+			alert("Este mes tiene 31 días.");
+			break;
+		case 2:
+			alert("Este mes tiene 30 días.");
+			break;
+		case 3:
+			alert("Este mes tiene 28 días.");
+			break;
+		default:
+			break;			
+	}
 
 
 
