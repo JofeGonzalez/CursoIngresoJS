73a4457379800395f04c0c561e6c06b4e64cea62
JofeGonzalez <60555982+JofeGonzalez@users.noreply.github.com>
2020-02-10 18:48:24 -0300

    if7

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,7 +1,16 @@
 function mostrar()
 {
 //tomo la edad  
+var edad;
+var estadoCivil;
 
+edad=document.getElementById('edad').value;
+estadoCivil=document.getElementById('estadoCivil').value;
+if(edad<18){
+	if(estadoCivil!='Soltero'){
+		alert("Es muy pequeño para NO ser soltero");
+	}
+ }
 	
 
 
