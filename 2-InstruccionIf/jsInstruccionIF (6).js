728282337d761bf68a700cd2cbe1efeba00c6c42
JofeGonzalez <60555982+JofeGonzalez@users.noreply.github.com>
2020-02-10 18:48:08 -0300

    if6

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,8 +1,18 @@
 function mostrar()
 {
 //tomo la edad  
+var edad;
 
-
+edad=document.getElementById('edad').value;
+if(edad<13){
+	alert("la persona es un niño");
+ }
+if(edad>=13 && edad<=17){
+	alert("la persona es un adolescente");
+ }
+ if(edad>17){
+	alert("la persona es un adulto");
+ }
 
 
 }//FIN DE LA FUNCIÓN
