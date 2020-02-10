140c53bbc42e28acb649998a65885ecb58c41852
JofeGonzalez <60555982+JofeGonzalez@users.noreply.github.com>
2020-02-10 18:54:22 -0300

    if10

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -2,5 +2,15 @@ function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	
-
+	var num;
+	num=Math.floor(Math.random()*10)+1;
+	if(num>=9){
+		alert("EXCELENTE");
+	}
+	if(num>=4 && num<9){
+		alert("APROBÓ");
+	}
+	if(num<4){
+		alert("Vamos, la proxima se puede")
+	}
 }//FIN DE LA FUNCIÓN
