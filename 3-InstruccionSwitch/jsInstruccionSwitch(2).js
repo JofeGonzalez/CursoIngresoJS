01ca6a3ee3578f1419f0f2f1185222bfa3156a12
JofeGonzalez <60555982+JofeGonzalez@users.noreply.github.com>
2020-02-10 20:41:20 -0300

    Switch2

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -1,10 +1,30 @@
 function mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+	var mesDelAño = document.getElementById('mes').value;
 
-alert (mesDelAño);
-
+	if(mesDelAño=='Enero' || mesDelAño=='Febrero' || mesDelAño=='Marzo' || mesDelAño=='Abril' || mesDelAño=='Mayo' || mesDelAño=='Junio'){
+		mesDelAño=1;	
+	}
+	if(mesDelAño=='Julio' || mesDelAño=='Agosto'){
+		mesDelAño=2;	
+	}
+	if(mesDelAño=='Septiembre' || mesDelAño=='Octubre' || mesDelAño=='Noviembre' || mesDelAño=='Diciembre'){
+		mesDelAño=3;	
+	}
+	switch(mesDelAño){
+		case 1:
+			alert("Falta para el invierno.");
+			break;
+		case 2:
+			alert("Abrigate que hace frio.");
+			break;
+		case 3:
+			alert("Ya pasamos el frio, ahora calor!!!.");
+			break;
+		default:
+			break;			
+	}
 
 
 
