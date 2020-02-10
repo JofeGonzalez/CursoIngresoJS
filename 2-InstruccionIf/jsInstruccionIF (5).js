c1247955bfb9c68ea6de5ce703075b73026bc350
JofeGonzalez <60555982+JofeGonzalez@users.noreply.github.com>
2020-02-10 18:47:42 -0300

    if5

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,11 @@
 function mostrar()
 {
 //tomo la edad  
+var edad;
 
+edad=document.getElementById('edad').value;
+if(edad<13 || edad>17){
+	alert("la persona NO es un adolescente");
+ }
 
-}//FIN DE LA FUNCIÓN
+}//FIN DE LA FUNCIÓN11
