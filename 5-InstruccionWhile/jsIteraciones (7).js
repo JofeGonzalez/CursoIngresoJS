2a73069115b202949d17988301288a4ff7596705
My Name <I@example.com>
2020-02-11 20:01:59 -0300

    While7

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -4,7 +4,18 @@ function mostrar()
 	var contador=0;
 	var acumulador=0;
 	var respuesta='si';
-
+	var num;
+	var opc;
+	opc=prompt("Quiere ingresar un numero?");
+	while(respuesta==opc){
+		num=prompt("Ingrese un numero");
+		num=parseInt(num);
+		contador++;
+		acumulador=acumulador+num;
+		if(respuesta==opc){
+			opc=prompt("Quiere ingresar otro numero?");
+		}
+	}
 
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/contador;
