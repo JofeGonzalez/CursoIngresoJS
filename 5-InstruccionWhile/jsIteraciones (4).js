e47bcd525098c7f8b8b1b97ec4536d852065b181
JofeGonzalez <60555982+JofeGonzalez@users.noreply.github.com>
2020-02-10 22:02:52 -0300

    While4

--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -2,6 +2,9 @@ function mostrar()
 {
 
 	var numero = prompt("ingrese un número entre 0 y 10.");
-
+	while(numero>-1 && numero<10){
+		alert("Numero Clave correcto.");
+		exit(1);
+	}
 
 }//FIN DE LA FUNCIÓN
