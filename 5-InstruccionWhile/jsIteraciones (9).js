1e7d2f33f03960bfc8e709417b1fec9eb3462ea5
My Name <I@example.com>
2020-02-11 20:38:05 -0300

    While9

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -3,15 +3,27 @@ function mostrar()
 
 	var contador=0;
 	// declarar variables
-	
+	var num;
+	var num1=0;
+	var num2=0;
 	var respuesta='si';
-
-	while(respuesta!='no')
-	{
-		
-	
+	var opc;
+	opc=prompt("Quiere ingresar un numero?");
+	while(respuesta==opc){
+		num=prompt("Ingrese un numero");
+		num=parseInt(num);
+		contador++;
+		if(num>num1){
+			num1=num;
+		} else{
+			num2=num;
+		}
+		if(respuesta==opc){
+			opc=prompt("Quiere ingresar otro numero?");
+		}
 	}
-
+	document.getElementById('maximo').value=num1;
+	document.getElementById('minimo').value=num2;
 
 
 
