f8b66123ccad9746ed048f7de4cc6aaa61b87c96
JofeGonzalez <60555982+JofeGonzalez@users.noreply.github.com>
2020-02-10 22:03:09 -0300

    While5

--- 5-InstruccionWhile/jsIteraciones (5).js
+++ 5-InstruccionWhile/jsIteraciones (5).js
@@ -1,10 +1,10 @@
 function mostrar()
 {
 
-var sexo = prompt("ingrese f ó m .");
-
-
-
-document.getElementById('Sexo').value=sexo;
+	var sexo = prompt("ingrese f ó m .");
+	while(sexo=='f' || sexo=='m'){
+	document.getElementById('Sexo').value=sexo;
+	exit(1);
+	}
 
 }//FIN DE LA FUNCIÓN
