d26dc5efd41a8435311eb8981c37871722ceb689
JofeGonzalez <60555982+JofeGonzalez@users.noreply.github.com>
2020-02-10 22:02:06 -0300

    While3

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -1,7 +1,10 @@
 function mostrar()
 {
 
-var clave = prompt("ingrese el número clave.");
-
+	var clave = prompt("ingrese el número clave.");
+	while(clave == 'utn750'){
+		alert("Entrada exitosa.");
+		exit(1);
+	}
 
 }//FIN DE LA FUNCIÓN
