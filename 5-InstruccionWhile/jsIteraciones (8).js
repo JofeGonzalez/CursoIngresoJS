ea87781db18afa6929b6d7e9672e207762550ec2
My Name <I@example.com>
2020-02-11 20:18:26 -0300

    While8

--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -6,6 +6,20 @@ function mostrar()
 	var negativo=1;
 	
 	var respuesta='si';
+	opc=prompt("Quiere ingresar un numero?");
+	while(respuesta==opc){
+		num=prompt("Ingrese un numero");
+		num=parseInt(num);
+		contador++;
+		if(num>0){
+			positivo=positivo+num;
+		} else{
+			negativo=negativo*num;
+		}
+		if(respuesta==opc){
+			opc=prompt("Quiere ingresar otro numero?");
+		}
+	}
 
 
 document.getElementById('suma').value=positivo;
