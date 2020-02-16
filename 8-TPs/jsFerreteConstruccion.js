e2804676059aac45ed77abdfe2f840319c6561aa
JofeGonzalez <j.gonzalez2299@gmail.com>
2020-02-16 20:23:37 -0300

    Ferrete Construccion

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -6,13 +6,38 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
-
+	var largo;
+	var ancho;
+	var resultado;
+	largo=document.getElementById('Largo').value;
+	largo=parseInt(largo);
+	ancho=document.getElementById('Ancho').value;
+	ancho=parseInt(ancho);
+	resultado=(largo*ancho*3);
+	alert("La cantidad de alambre necesaria para el terreno rectangular es: " + resultado);
 }
 function Circulo () 
 {
-	
+	var radio;
+	var resultado;
+	radio=document.getElementById('Radio').value;
+	radio=parseInt(radio);
+	resultado=((2*3.14*radio)*3);
+	alert("La cantidad de alambre necesaria para el terreno circular es: " + resultado)
 }
 function Materiales () 
 {
-	
+	var largo;
+	var ancho;
+	var BolsasCemento;
+	var BolsasCal;
+	var resultado;
+	largo=document.getElementById('Largo').value;
+	largo=parseInt(largo);
+	ancho=document.getElementById('Ancho').value;
+	ancho=parseInt(ancho);
+	resultado=(largo*ancho);
+	BolsasCemento=(resultado/2);
+	BolsasCal=(resultado/3);
+	alert("La cantidad de bolsas de cemento necesarias es: "+BolsasCemento+" y la cantidad de bolsas de cal es: "+BolsasCal+".");
 }
