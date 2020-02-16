b28d9a63ff2705325f360daa7c27c849f0562d26
JofeGonzalez <j.gonzalez2299@gmail.com>
2020-02-16 19:33:17 -0300

    Ferrete Facturacion

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -5,13 +5,52 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+	var Precio1;
+	var Precio2;
+	var Precio3;
+	var resultado;
+
+	Precio1=document.getElementById('PrecioUno').value;
+	Precio1=parseInt(Precio1);
+	Precio2=document.getElementById('PrecioDos').value;
+	Precio2=parseInt(Precio2);
+	Precio3=document.getElementById('PrecioTres').value;
+	Precio3=parseInt(Precio3);
+	resultado=Precio1+Precio2+Precio3;
+	alert("La suma de los productos es: " + resultado);
 }
+
 function Promedio () 
 {
-	
+	var Precio1;
+	var Precio2;
+	var Precio3;
+	var resultado;
+
+	Precio1=document.getElementById('PrecioUno').value;
+	Precio1=parseInt(Precio1);
+	Precio2=document.getElementById('PrecioDos').value;
+	Precio2=parseInt(Precio2);
+	Precio3=document.getElementById('PrecioTres').value;
+	Precio3=parseInt(Precio3);
+	resultado=Precio1+Precio2+Precio3;
+	alert("La suma de los productos es: " + resultado/3);
 }
 function PrecioFinal () 
 {
-	
+	var Precio1;
+	var Precio2;
+	var Precio3;
+	var resultado;
+	var resultadoFinal;
+
+	Precio1=document.getElementById('PrecioUno').value;
+	Precio1=parseInt(Precio1);
+	Precio2=document.getElementById('PrecioDos').value;
+	Precio2=parseInt(Precio2);
+	Precio3=document.getElementById('PrecioTres').value;
+	Precio3=parseInt(Precio3);
+	resultado=(Precio1+Precio2+Precio3);
+	resultadoFinal=(resultado+(resultado*21/100));
+	alert("La suma de los productos es: " + resultadoFinal);
 }
