46857a5a869fbb6bd5b1e852f45b7efa5c9eaed1
alumno <alumno@LAB00PC00>
2020-02-17 21:30:09 -0300

    Switch2

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -2,24 +2,23 @@ function mostrar()
 {
 //tomo la edad  
 	var mesDelAño = document.getElementById('mes').value;
-
-	if(mesDelAño=='Enero' || mesDelAño=='Febrero' || mesDelAño=='Marzo' || mesDelAño=='Abril' || mesDelAño=='Mayo' || mesDelAño=='Junio'){
-		mesDelAño=1;	
-	}
-	if(mesDelAño=='Julio' || mesDelAño=='Agosto'){
-		mesDelAño=2;	
-	}
-	if(mesDelAño=='Septiembre' || mesDelAño=='Octubre' || mesDelAño=='Noviembre' || mesDelAño=='Diciembre'){
-		mesDelAño=3;	
-	}
 	switch(mesDelAño){
-		case 1:
+		case 'Enero':
+		case 'Febrero':
+		case 'Marzo':
+		case 'Abril':
+		case 'Mayo':
+		case 'Junio':
 			alert("Falta para el invierno.");
 			break;
-		case 2:
+		case 'Julio':
+		case 'Agosto':
 			alert("Abrigate que hace frio.");
 			break;
-		case 3:
+		case 'Septiembre':
+		case 'Octubre':
+		case 'Noviembre':
+		case 'Diciembre':
 			alert("Ya pasamos el frio, ahora calor!!!.");
 			break;
 		default:
