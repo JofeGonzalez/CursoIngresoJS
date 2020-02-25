84c306df2a1c365c58a04d242523773012a4282d
JofeGonzalez <j.gonzalez2299@gmail.com>
2020-02-25 11:39:36 -0300

    Switch6

--- 3-InstruccionSwitch/jsInstruccionSwitch(6).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(6).js
@@ -2,23 +2,36 @@ function mostrar()
 {
 //tomo la edad  
 	var laHora = document.getElementById('hora').value;
-	if(laHora>-1 && laHora<7 || laHora>19 && laHora<25){
-			laHora=1;
-	}
-	if(laHora>6 && laHora<12){
-			laHora=2;
-	}
-	if(laHora>11 && laHora<20){
-			laHora=3;
-	}	
 	switch(laHora){
-		case 1:
+		case '0':
+		case '1':
+		case '2':
+		case '3':
+		case '4':
+		case '5':
+		case '6':
+		case '20':
+		case '21':
+		case '22':
+		case '23':
+		case '24':
 			alert("Es de noche.");
 			break;
-		case 2:
+		case '7':
+		case '8':
+		case '9':
+		case '10':
+		case '11':
 			alert("Es de mañana.");
 			break;	
-		case 3:
+		case '12':
+		case '13':
+		case '14':
+		case '15':
+		case '16':
+		case '17':
+		case '18':
+		case '19':
 			alert("Es de tarde.");
 			break;	
 		default:
