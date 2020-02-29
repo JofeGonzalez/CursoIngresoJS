27fb1f2eda284bd867703c2a349fd7cd9ca29f7d
JofeGonzalez <j.gonzalez2299@gmail.com>
2020-02-29 16:40:41 -0300

    For3

--- 6-InstruccionFor/jsIteracionesFor (3).js
+++ 6-InstruccionFor/jsIteracionesFor (3).js
@@ -1,8 +1,11 @@
 function mostrar()
 {
 
-var repetciones = prompt("ingrese el número de repeticiones");
+	var repetciones = prompt("ingrese el número de repeticiones");
+	var i;
 
-
+	for(i=0; i<repetciones; i++){
+		alert("Hola UTN FRA");
+	}
 
 }//FIN DE LA FUNCIÓN
