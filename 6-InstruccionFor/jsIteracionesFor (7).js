1355e5ffa7fb4e7b100ee70f6a7ed3d81dceeade
JofeGonzalez <j.gonzalez2299@gmail.com>
2020-02-29 17:00:59 -0300

    For7

--- 6-InstruccionFor/jsIteracionesFor (7).js
+++ 6-InstruccionFor/jsIteracionesFor (7).js
@@ -1,6 +1,18 @@
 function mostrar()
 {
-
+		var i;
+	var num;
+	var cont=0;
+	num=prompt("ingrese un numero");
+	num=parseInt(num);
+	for(i=1; i<=num; i++)
+	{
+		if(num%i==0)
+		{
+			cont++;
+		}
+	}
+	alert("La cantidad de numeros pares encontrados es: " + cont);
 
 
 
