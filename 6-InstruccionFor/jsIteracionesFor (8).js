cf42b129457d97653c62e67715ac6e43f22dae8a
JofeGonzalez <j.gonzalez2299@gmail.com>
2020-02-29 20:08:25 -0300

    For8

--- 6-InstruccionFor/jsIteracionesFor (8).js
+++ 6-InstruccionFor/jsIteracionesFor (8).js
@@ -1,7 +1,22 @@
 function mostrar()
 {
-
-
-
-
-}//FIN DE LA FUNCIÓN
+	var i;
+	var num;
+	var cont=0;
+	num=prompt("ingrese un numero");
+	num=parseInt(num);
+	for(i=1; i<=num; i++)
+	{
+		if(num%i==0)
+		{
+			cont++;
+		}
+	}
+	if(cont==2){
+	alert("El numero es Primo");	
+	}
+	else
+	{
+		alert("El numero no es primo");
+	}
+}
