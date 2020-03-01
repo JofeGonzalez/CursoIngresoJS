7c3d062647094a7a5cc660e6b82f4a1e175f623b
JofeGonzalez <j.gonzalez2299@gmail.com>
2020-03-01 00:34:09 -0300

    Ejercicio 2

--- 9-Parciales/2019_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2019_1Cuatri_Parcial_fuentes/dos.js
@@ -1,4 +1,16 @@
 function mostrar()
 {
-  
+  var nombre1;
+  var nombre2;
+  var peso1;
+  var peso2;
+  var sumaPesos;
+  var promedioPesos;
+  nombre1=prompt("Ingrese el primer nombre: ");
+  peso1=parseInt(prompt("Ingrese el primer peso"));
+  nombre2=prompt("Ingrese el segundo nombre: ");
+  peso2=parseInt(prompt("Ingrese el segundo peso"));
+  sumaPesos=peso1+peso2;
+  promedioPesos=sumaPesos/2;
+  alert("Ustedes se llaman " +nombre1+ " y " +nombre2+ " pesan " +peso1+ " y " +peso2+ " ,que sumados son " +sumaPesos+ " kilos y el promedio de peso " +promedioPesos);
 }
