1d80152681017ff0a6c53bd88ee13cadce941740
JofeGonzalez <j.gonzalez2299@gmail.com>
2020-03-01 00:33:53 -0300

    Ejercicio 4

--- 9-Parciales/2019_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2019_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,29 @@
 function mostrar()
 {
+	var num1;
+	var num2;
+	var suma;
+	var resta;
 
+	num1=prompt("Ingrese el primer numero");
+	num2=prompt("Ingrese el segundo numero");
+	if(num1==num2)
+	{
+		alert(num1+num2);
+	} else
+	{
+		num1=parseInt(num1);
+		num2=parseInt(num2);
+		if(num1>num2)
+		{
+			resta=num1-num2;
+			if(resta>10){
+				alert("la resta es " +resta+ " y superó el 10");
+			}
+		} else
+		{
+			suma=num1+num2;
+			alert(suma);
+		}
+	}
 }
