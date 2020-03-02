1d746504103e0a34064a663e6dd044d133947503
alumno <alumno@LAB00PC00>
2020-03-02 19:28:05 -0300

    Ejercicio 3

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,10 @@
 function mostrar()
 {
-
+	var precio;
+	var descuento;
+	var precioFinal;
+	precio=parseInt(prompt("Ingrese el precio inicial"));
+	descuento=parseInt(prompt("Ingrese el porcentaje de descuento"));
+	precioFinal=precio-precio*descuento/100;
+	document.getElementById('elPrecioFinal').value=precioFinal;
 }
