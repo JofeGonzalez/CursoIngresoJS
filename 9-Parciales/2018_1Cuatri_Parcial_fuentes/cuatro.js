f15b70a995bd53d0e5390ab0f409bd52c197d3ef
alumno <alumno@LAB00PC00>
2020-03-02 19:28:13 -0300

    Ejercicio 4

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,29 @@
 function mostrar()
 {
+	var num1;
+	var num2;
+	var suma;
+	var resta;
 
+	num1=prompt("Ingrese el primer numero");
+	num2=prompt("Ingrese el segundo numero");
+
+	if(num1==num2)
+	{
+		alert("La concatenacion de los numeros es: "+num1+num2);
+	} else
+	{
+		num1=parseInt(num1);
+		num2=parseInt(num2);
+		if(num1>num2)
+		{
+			resta=num1-num2;
+			alert("La resta de los numeros es: "+resta);
+		} else
+		{
+			suma=num1+num2;
+			if(suma>10);
+				alert("La suma es: " +suma+ " y supero el 10");
+		}
+	}
 }
