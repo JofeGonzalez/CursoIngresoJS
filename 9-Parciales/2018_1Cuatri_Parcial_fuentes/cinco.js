06ecd8e185dd6f39ab861f4d2e2a964730cba68b
alumno <alumno@LAB00PC00>
2020-03-02 19:28:21 -0300

    Ejercicio 5

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cinco.js
@@ -1,4 +1,26 @@
 function mostrar()
 {
+	var planeta;
+	planeta=prompt("Ingrese un planeta del sistema solar");	
+	switch(planeta)
+	{
+		case 'tierra':
+			alert("acá vivimos");
+			break;
+		case 'mercurio':
+		case 'venus':
+			alert("acá hace mas calor");
+			break;
+		case 'marte':
+		case 'jupiter':
+		case 'saturno':
+		case 'urano':
+		case 'neptuno':
+			alert("acá hcae más frío");
+			break;
+		default:
+			alert("este no es un planeta valido");
+			break;	
 
+	}
 }
