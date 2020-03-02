de60db7f57ea27248de9ddc0c90522932bb2064a
alumno <alumno@LAB00PC00>
2020-03-02 19:27:51 -0300

    Ejercicio 1

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,12 @@
 
 function mostrar()
 {
+	var altura;
+	var base;
+	var perimetro;
 
+	altura=parseInt(prompt("Ingrese la altura del rectangulo"));
+	base=parseInt(prompt("Ingrese la base del rectangulo"));
+	perimetro=(base+base+altura+altura);
+	alert("El perimetro del rectangulo es: " +perimetro);
 }
