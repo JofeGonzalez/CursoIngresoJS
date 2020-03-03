0c2a2a57b796147675f4cc4d52d269416473854f
alumno <alumno@LAB00PC00>
2020-03-02 22:22:04 -0300

    Ejercicio 9

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -1,4 +1,52 @@
 function mostrar()
 {
+	var marca;
+	var peso;
+	var temperatura;
+	var cantTempPares=0;
+	var SumaPeso=0;
+	var MarcaPesoMax;
+	var cantProductosMenosCero=0;
+	var cantProductos=0;
+	var PesoMax=1;
+	var PesoMin=100;
+	var respuesta;
+
+	do
+	{
+		marca=prompt("Ingrese la marca del producto");
+		do
+		{
+			peso=parseInt(prompt("Ingrese el peso del producto"));
+		}while(peso<1 || peso>100);
+		do
+		{
+			temperatura=parseInt(prompt("Ingrese la temperatura de almacenamiento del producto"));
+		}while(temperatura<-30 || temperatura>30);
+		cantProductos++;
+		SumaPeso=SumaPeso+peso;
+		if(temperatura%2==0 && temperatura!=0)
+		{
+			cantTempPares++;
+		}
+		if(peso>PesoMax)
+		{
+			PesoMax=peso;
+			MarcaPesoMax=marca;
+		}
+		if(temperatura<0)
+		{
+			cantProductosMenosCero++;
+		}
+		if(peso<PesoMin)
+		{
+			PesoMin=peso;
+		}
+		respuesta=prompt("Quiere ingresar otro dato?");
+	}while(respuesta=='s');
+	document.write("La cantidad de temperaturas pares es :" +cantTempPares);
+	document.write("<br>La marca del producto más pesado es: " +MarcaPesoMax);
+	document.write("<br>La cantidad de productos que se conservan a menos de 0 grados es: " +cantProductosMenosCero);
+	document.write("<br>El peso  maximo es: " +PesoMax+ " y el peso minimo es: " +PesoMin);
 
 }
